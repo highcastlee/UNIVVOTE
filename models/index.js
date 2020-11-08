c924735bfd5fe8c264a0615a237dbdcd011fa05c
highcastle4242 <wnstjd4242@naver.com>
2020-11-09 02:30:58 +0900

    fix

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const Major = require('./major');
 const Like = require('./like');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const config = require('../config/config')[production];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
