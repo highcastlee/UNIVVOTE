0e94a819724140db91d5bf3f9202151dc9666b9a
highcastle4242 <wnstjd4242@naver.com>
2020-11-09 16:36:22 +0900

    fix: config

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const Major = require('./major');
 const Like = require('./like');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')['development'];
+const config = require('../config/config')[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
